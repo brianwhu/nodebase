18a6129523e7c0801c14a93a2c8beab803b5f2a1
Brian Hu <brianwhu@gmail.com>
2023-10-17 21:44:14 -0400

    terminal: adding unit tests

--- terminal/test/prompt.js
+++ terminal/test/prompt.js
@@ -2,10 +2,10 @@ import assert from "node:assert"
 import terminal from "../index.js"
 import generate from "./generate.js"
 
-const get = async (keys, done) => {
+const get = async (keys, terminator, done) => {
     const prompt = terminal.prompt("")
     keys.split('').forEach(key => process.stdin.emit('data', key))
-    process.stdin.emit('data', '\n')
+    process.stdin.emit('data', terminator)
     done()
     return await prompt;
 }
@@ -13,13 +13,48 @@ const get = async (keys, done) => {
 describe('Terminal', () => {
   describe('#prompt()', () => {
 
+    process.stderr.write = string => {}
+
     it('should receive all keyboard input', async done => {
       const keys = generate(1, 16, 0, 0)
       if (process.env.VERBOSE) {
         console.log(`Sending keys '${keys}'`)
       }
-      let secret = await get(keys, done)
+      let secret = await get(keys, '\n', done)
       assert.strictEqual(secret, keys, `Expecting '${keys}' but got '${secret}'`)
+      assert.strictEqual(process.stdin.isRaw, false)
+    })
+
+    it('should handle explicit ^D and ignore the rest of the input', async done => {
+      const keys = generate(1, 16, 0, 0) + '\u0004'
+      const extra = generate(1, 16, 0, 0)
+      if (process.env.VERBOSE) {
+        console.log(`Sending keys '${keys + extra}'`)
+      }
+      let secret = await get(keys + extra, '\n', done)
+      assert.strictEqual(secret, keys, `Expecting ${keys} but got '${secret}'`)
+    })
+
+    it('should handle BACKSPACE', async done => {
+      const keys = generate(1, 16, 0, 0) + '\u007f'
+      if (process.env.VERBOSE) {
+        console.log(`Sending keys '${keys}'`)
+      }
+      let secret = await get(keys, '\n', done)
+      assert.strictEqual(secret, keys.substring(1, key.length - 1), `Expecting ${keys.substring(1, key.length - 1)} but got '${secret}'`)
+    })
+
+    it('should handle explicit ^C, get interrupted, and receive nothing', async done => {
+      const keys = generate(1, 16, 0, 0)
+      if (process.env.VERBOSE) {
+        console.log(`Sending keys '${keys}' then ^C`)
+      }
+      let interrupted = false
+
+      process.on('SIGINT', () => interrupted = true)
+      let secret = await get(keys, '\u0003', done)
+      assert.strictEqual(interrupted, true)
+      assert.strictEqual(secret, "", `Expecting <nothing> but got '${secret}'`)
     })
 
   })
